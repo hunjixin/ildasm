59b854b72d1c35f03c995488bfe5ec7d100de25f
hunjixin <1084400399@qq.com>
2017-07-20 18:21:11 +0800

    initlize project

---
+++ dev/main.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow ,Menu} from 'electron'
+import path from 'path'
+import url from 'url'
+import {template} from './MenuTemplate.js'
+let mainWindow
+
+app.on('ready', ()=> {
+  var menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  createWindow()
+})
+
+app.on('window-all-closed', ()=>  {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', ()=>  {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600,icon:'images/sd.icon'})
+  mainWindow.webContents.openDevTools()
+  mainWindow.loadURL(url.format({
+    pathname: "/pub/index.html",
+    protocol: 'file:',
+    slashes: true
+  }))
+  mainWindow.on('closed', ()=>{ mainWindow = null})
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+
+// const ExtractCss = new ExtractTextPlugin('style/[name]_[hash:8].css')
+// const ExtractLess = new ExtractTextPlugin('style/[name]_[hash:8].css')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    main: './dev/main.js',
+    index: './dev/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'pub'),
+    publicPath: '/pub/',
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {loader: 'html-loader'}
+        ]
+
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function () {
+                  return [
+                    autoprefixer({
+                      broswers: ['last 5 versions']
+                    })
+                  ]
+                }
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.js|jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: 'body',
+      title: 'Ilasm',
+      chunks: ['index']
+    }),
+    new ExtractTextPlugin('style/[name]_[hash:8].css')
+  ],
+  devServer: {
+    contentBase: __dirname + '/pub'
+  },
+  target: 'electron'
+}
